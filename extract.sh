df53ad4f7ef33466df55810609e9cbb072c8713c
Dominic Chen <d.c.ddcc@gmail.com>
2017-01-10 17:48:41 -0500

    Use tmpfs for docker, support spaces in filenames, change image

--- extract.sh
+++ extract.sh
@@ -3,15 +3,18 @@
 infile=$1
 outdir=$2
 
-indir=$(realpath $(dirname ${infile}))
-outdir=$(realpath ${outdir})
-infilebn=$(basename ${infile})
+# override 65535k docker default size with tmpfs default
+mem=$(($(free | awk '/^Mem:/{print $2}') / 2))k
 
-docker run --rm -t -i \
-  -v ${indir}:/firmware-in:ro \
-  -v ${outdir}:/firmware-out \
-  "phretor/firmware-extractor:latest" \
+indir=$(realpath $(dirname "${infile}"))
+outdir=$(realpath "${outdir}")
+infilebn=$(basename "${infile}")
+
+docker run --rm -t -i --tmpfs /tmp:rw,size=${mem} \
+  -v "${indir}":/firmware-in:ro \
+  -v "${outdir}":/firmware-out \
+  "ddcc/firmadyne-extractor:latest" \
   fakeroot /home/extractor/extractor/extractor.py \
   -np \
-  /firmware-in/${infilebn} \
+  /firmware-in/"${infilebn}" \
   /firmware-out
